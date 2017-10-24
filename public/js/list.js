204ef20cec904a7e8d3bae4173af960ed64bf71e
touchsky <964211422@qq.com>
2017-10-24 23:58:12 +0800

    list

--- public/js/list.js
+++ public/js/list.js
@@ -106,13 +106,31 @@ var List = (function ($) {
                     // });
                 },
                 'resetRoomData' : function(list){   //重置会议室时间数据
+                    var _this = this;
                     list.forEach(function(item){
                         item.timeArr = new Array(item.allTimeListCount);
-                        item.startNum = parseInt(item.openTimeStart);
-                        item.checkArr = [];
+                        item.startNum = parseInt(item.openTimeStart);   //开始时间的数值
+                        item.checkArr = []; //选取的时间数组
+                        item.cTimeStr = '';
+                        item.CTimeNum = 0;
                         item.cfirst = '';   //第一个选择的时间
+
+                        //如果选中的日期是今天,重置有效选择数值canReserveList
+                        (_this.setDay == _this.today)&&(new Date().getHours()>=item.startNum)&&(item.canReserveList = _this.resetCanTArr(item));
                     })
                     this.rooms = list;
+                },
+                'resetCanTArr' : function(item){
+                    //获取当前时间
+                    var date = new Date();
+                    var h = date.getHours();
+                    var m = date.getMinutes() > 30 ? 2 : 1;
+                    var max = (h - item.startNum) * 2 + m;
+                    var arr = [];
+                    item.canReserveList.forEach(function(item){
+                        (item>=max) && (arr.push(item));
+                    })
+                    return arr;
                 },
                 'checkTime' : function(index,t){   //时间段选取
                     if(this.rooms[index].canReserveList.indexOf(t) < 0){
@@ -122,16 +140,56 @@ var List = (function ($) {
                             this.rooms[index].cfirst = t;
                             this.rooms[index].checkArr.push(t);
                         }else{
-                            (this.rooms[index].cfirst == t)&&(this.rooms[index].cfirst='',this.rooms[index].checkArr=[]);
-                            if(t > this.rooms[index].cfirst){
+                            if(this.rooms[index].cfirst == t){
+                                this.rooms[index].cfirst='';
+                                this.rooms[index].checkArr=[];
+                            }else if(t > this.rooms[index].cfirst){
                                 //判断选中是否含有不可选的时间段
-                                var isvalid = false,cArr = [];
+                                var isvalid = true,cArr = [];
                                 for(var x = this.rooms[index].cfirst; x<=t; x++){
-
+                                    if(this.rooms[index].canReserveList.indexOf(x) < 0){
+                                        isvalid = false;
+                                        return false;
+                                    }
+                                    cArr.push(x);
                                 }
+                                (isvalid)&&(this.rooms[index].checkArr = cArr)
+                            }else{
+                                //判断选中是否含有不可选的时间段
+                                var isvalid = true,cArr = [];
+                                for(var x = t; x<=this.rooms[index].cfirst; x++){
+                                    if(this.rooms[index].canReserveList.indexOf(x) < 0){
+                                        isvalid = false;
+                                        return false;
+                                    }
+                                    cArr.push(x);
+                                }
+                                (isvalid)&&(this.rooms[index].checkArr = cArr)
                             }
                         }
                     }
+                    this.calCheckTime(index);
+                },
+                'calCheckTime' : function(index){    //计算选择时间段
+                    if(this.rooms[index].checkArr.length <=0){
+                        this.rooms[index].CTimeNum = 0;
+                        this.rooms[index].cTimeStr = '';
+                        return false;
+                    }
+                    var hours = this.rooms[index].checkArr.length / 2;
+                    this.rooms[index].CTimeNum = hours.toFixed(1);
+
+                    var sNum = this.rooms[index].startNum;  //开始时间的数值
+                    var len = this.rooms[index].checkArr.length;
+                    var cStart = this.rooms[index].checkArr[0];
+                    var csNum = parseInt(cStart / 2);
+                    var csStr = (cStart%2==0) ? (sNum + csNum) + ':00' : (sNum + csNum) + ':30';
+
+                    var cEnd = this.rooms[index].checkArr[len-1]
+                    var ceNum = parseInt(cEnd / 2);
+                    var ceStr = (cStart%2!=0) ? (sNum + ceNum) + ':00' : (sNum + ceNum) + ':30';
+
+                    this.rooms[index].cTimeStr = csStr + '-' + ceStr;
                 },
                 'resetDate' : function(){
                     //获取当前日期
