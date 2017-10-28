25a3f2dc12a1456c9b13027c7cbe4c34abf23cf2
刘瑶 <307956355@qq.com>
2017-10-28 16:31:59 +0800

    fix

--- routes/coupon.js
+++ routes/coupon.js
@@ -1,23 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var Coupon = {
+    'id': 1234,
+    'basicPrice' : 30,
+    'limit': 200,
+    'couponName' : '满200减20',
+    'timeStart' : 1508566101200,
+    'timeEnd' : 1508566101200,
+    'getTime':1508566101200
+};
+
 /* GET home page. */
 router.get('/loadUserCoupon', function(req, res, next) {
-	var array = [
-        {
-            'id': 1234,
-            'basicPrice' : 30,
-            'limit': 200,
-            'couponName' : '满200减20',
-            'timeStart' : 1508566101200,
-            'timeEnd' : 1508566101200
-        },{
+	var array = [Coupon,{
             'id': 12345,
             'basicPrice' : 20,
             'limit': 0,
             'couponName' : '满200减20',
             'timeStart' : 1508566101200,
-            'timeEnd' : 1508566101200
+            'timeEnd' : 1508566101200,
+            'getTime':1508566101200
         }
     ];
     var data = {success:true};
